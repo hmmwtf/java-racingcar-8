26a78cbddf0f47a2fd9e3dd5c7a03263f786177e
hmmwtf <94499659+hmmwtf@users.noreply.github.com>
2025-10-26 14:58:28 +0900

    feat(view): add methods to display race results and winners
    Implemented `printExecutionMessage`, `printRoundResult`, and `printWinner` methods in `OutputView` to handle race result output and display final winners. Enhanced user feedback mechanism.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,23 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+import java.util.List;
+
 public class OutputView {
+    public static void printExecutionMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void printRoundResult(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(car);
+        }
+        System.out.println();
+    }
+
+    public static void printWinner(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
 }
